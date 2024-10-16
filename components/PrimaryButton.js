d8d4528beca77c6900c9f371ae79d26544eca82c
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-10-16 15:29:40 +0200

    feat: linear gradient added

---
+++ components/PrimaryButton.js
@@ -0,0 +1,46 @@
+import { StyleSheet, View, Text, Pressable } from 'react-native'
+
+const PrimaryButton = ({ children }) => {
+    const pressHandler = () => {
+        console.log('Pressed!')
+    }
+    return (
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                onPress={pressHandler}
+                android_ripple={{ color: '#8d4f6e' }}
+                style={({ pressed }) =>
+                    pressed
+                        ? [styles.buttonInnerContainer, styles.pressed]
+                        : styles.buttonInnerContainer
+                }
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden',
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+
+        elevation: 2,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+    },
+    pressed: {
+        opacity: 0.75,
+    },
+})
+
+export default PrimaryButton
