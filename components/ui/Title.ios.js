830fa3b467edbc8a2d8f30c3719e1e1cfcd97b8d
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-10-23 15:31:11 +0200

    all done

---
+++ components/ui/Title.ios.js
@@ -0,0 +1,30 @@
+import { Platform, StyleSheet, Text, View } from 'react-native'
+
+const Title = ({ children }) => {
+    return (
+        <View>
+            <Text style={styles.title}>{children}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 24,
+        margin: 24,
+        fontFamily: 'open-sans-bold',
+        color: 'white',
+        textAlign: 'center',
+        // borderWidth: Platform.OS === 'ios' ? 2 : 0,
+        borderWidth: Platform.select({
+            android: 2,
+            ios: 0,
+        }),
+        borderColor: 'white',
+        padding: 12,
+        maxWidth: '80%',
+        minWidth: 300,
+    },
+})
+
+export default Title
